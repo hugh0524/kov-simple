ca40235c30997a46d68b465669590d0a6c186ae8
yinhe <yinhe@creditease.cn>
2018-07-24 23:27:37 +0800

    新增简单路由

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@
  */
 
 
-define(["pageInit", "./page/index"], function(page, index) {
-    
-    page(index, "index_demo")
+define(["pageInit", "./page/index", "./page/second"], function(page, index, second) {
+
+    var queryStringToObject = function (str) {
+        return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = decodeURI(n[1]),this}.bind({}))[0];
+    }
+
+    var pages = {
+        "index": index,
+        "second": second
+    }
+    var query = queryStringToObject()["page"]
+
+    page(pages[query || "index"], query+"_demo")
     
     
 })
